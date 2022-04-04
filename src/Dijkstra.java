f4f7758a4b0ea912f207efdfe5978bb431cad5d3
Lee Subin <nnibuseel@naver.com>
2022-04-04 14:28:07 +0900

    init

--- src/Dijkstra.java
+++ src/Dijkstra.java
@@ -1,22 +1,23 @@
 import java.util.Collections;
 
-public class Dijkstra {
-    private final int[][] graph;
-    private final boolean[] visited;
-    public static final int INF = Integer.MAX_VALUE / 2;
+public class Dijkstra {                                    //아는것
+    private final int[][] graph;                           //문제
+    private final boolean[] visited;                       //방문정보
+    public static final int INF = Integer.MAX_VALUE / 2;   //무한대
 
     public Dijkstra(int[][] graph) {
         this.graph = graph;
         this.visited = new boolean[graph.length];
     }
 
-    public int[] find(int x) {
-        int[] distance = graph[x];
+    //하는것
+    public int[] find(int x) {      //사작 정점
+        int[] distance = graph[x];  //distance[0,5,INF]
         visited[x] = true;
 
         for (int i = 0; i < graph.length; i++) {
-            int minimum = closest(graph[x]);
-            update(distance, minimum);
+            int minimum = closest(graph[x]);   //경유지 선택
+            update(distance, minimum);         //min(직행, 경유지)
         }
         return distance;
     }
@@ -46,7 +47,10 @@ public class Dijkstra {
         int[][] graph = {
                 {0,7,INF, INF, 3, 10, INF},
                 {7,0,3,10, 2,6,INF},
-                {INF, 4,0,2, INF,INF,INF}
+                {INF, 4,0,2, INF,INF,INF},
+                {3,2,INF,11,0,INF,5},
+                {10,6, INF, 9,INF, 0,INF, INF},
+                {INF, INF, INF, 4,5, INF, 0}
         };
         Dijkstra dijkstra = new Dijkstra(graph);
         int[] distance = dijkstra.find(0);
@@ -56,3 +60,5 @@ public class Dijkstra {
         System.out.println();
     }
 }
+
+//by Leesuuuuu
